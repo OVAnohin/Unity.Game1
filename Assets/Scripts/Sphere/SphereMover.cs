886db9f0815b343a085a75d8b1ee62bb4fdd588b
Oleg Anohin <oanohin@outlook.com>
2020-09-27 20:49:21 +0400

    time save

--- Assets/Scripts/Sphere/SphereMover.cs
+++ Assets/Scripts/Sphere/SphereMover.cs
@@ -2,7 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+
 public class SphereMover : MonoBehaviour
 {
+  [SerializeField] private float _speed;
+  [SerializeField] private float _zAngle;
+  [SerializeField] private float _jumpForce;
 
+  private Rigidbody _rigidbody;
+
+  private void Awake()
+  {
+    _rigidbody = GetComponent<Rigidbody>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetKey(KeyCode.Space) && _rigidbody.velocity == Vector3.zero)
+    {
+      _rigidbody.velocity = Vector3.up * _jumpForce;
+    }
+
+    transform.position = transform.position + Vector3.right * _speed * Time.deltaTime;
+    transform.Rotate(0, 0, _zAngle);
+
+  }
 }
