1ec909474b3f616e4c418dbe539082fa3193fc25
Oleg Anohin <oanohin@outlook.com>
2020-10-20 22:52:46 +0400

    New GroundGenerator

--- Assets/Scripts/Camera/Follow.cs
+++ Assets/Scripts/Camera/Follow.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-    [SerializeField] private Transform _camera;
-    [SerializeField] private float _deltaX;
+    [SerializeField] private Transform _selfTransform;
+    [SerializeField] private float _offsetY;
+    [SerializeField] private float _offsetZ;
+
+    private void Start()
+    {
+        _offsetY = _target.position.y + _offsetY;
+    }
 
     private void LateUpdate()
     {
-        _camera.position = new Vector3(_target.position.x + _deltaX, _camera.position.y, _camera.position.z);
+        _selfTransform.position = new Vector3(_target.position.x, _offsetY, _offsetZ);
     }
 }
--- Assets/Scripts/Ground/GroundGenerator.cs
+++ Assets/Scripts/Ground/GroundGenerator.cs
@@ -5,45 +5,19 @@ using UnityEngine;
 public class GroundGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject _groundPrefab;
-    [SerializeField] private GameObject _coinPrefab;
-    [SerializeField] private GameObject _boulderPrefab;
     [SerializeField] private Transform _groundPool;
     [SerializeField] private Sphere _player;
-    [SerializeField] private int _x;
-    [SerializeField] private int _z;
-    [SerializeField] private int _coin;
-    [SerializeField] private int _boulder;
+    [SerializeField] private int _lengthPlayingField;
+    [SerializeField] private int _widthPlayingField;
 
-    private int _startX;
-    private int _startZ;
-    private int _nextStep = 1;
     private Queue<GameObject> _grounds = new Queue<GameObject>();
-    private Queue<GameObject> _layerOnGrounds = new Queue<GameObject>();
-    private int _deltaX;
-    private int _lastX;
+    private int _nextStep = 1;
 
     private void Start()
     {
-        SetupVariables();
-
-        HashSet<int> positionsCoin = new HashSet<int>();
-        positionsCoin = FillPosition(positionsCoin, _coin);
-        HashSet<int>  positionsBoulder = FillPosition(positionsCoin, _boulder);
-
-        int i = 0;
-        for (int x = _startX; x < _x; x++)
-            for (int z = _startZ; z < _z; z++)
-            {
-                _grounds.Enqueue(GenerateGround(_groundPrefab, x, 0, z));
-
-                if (positionsCoin.Contains(i))
-                    _layerOnGrounds.Enqueue(GenerateGround(_coinPrefab, x, 1, z));
-
-                if (positionsBoulder.Contains(i))
-                    _layerOnGrounds.Enqueue(GenerateGround(_boulderPrefab, x, 1, z));
-
-                i += 1;
-            }
+        for (int x = -_lengthPlayingField / 2; x <= _lengthPlayingField / 2; x++)
+            for (int z = -_widthPlayingField / 2; z < _widthPlayingField / 2; z++)
+                _grounds.Enqueue(GenerateGround(_groundPrefab, x, z));
     }
 
     private void Update()
@@ -51,68 +25,24 @@ public class GroundGenerator : MonoBehaviour
         if (_player.transform.position.x >= _nextStep)
         {
             _nextStep += 1;
-
-            while ((_grounds.Peek().transform.position.x * -1) + _lastX == _deltaX)
-                MoveTailToHead(_grounds, _grounds.Peek().transform.position.z);
-
-            HashSet<int> zPositions = new HashSet<int>();
-            while ((_layerOnGrounds.Peek().transform.position.x * -1) + _lastX == _deltaX)
-            {
-                int zPosition = GenerateNewZPosition(zPositions);
-                MoveTailToHead(_layerOnGrounds, zPosition);
-            }
-
-            _lastX += 1;
+            MoveGround();
         }
     }
 
-    private GameObject GenerateGround(GameObject prefab, int x, int y, int z)
+    private void MoveGround()
+    {
+        for (int i = 0; i < _widthPlayingField; i++)
+        {
+            GameObject element = _grounds.Dequeue();
+            element.transform.position = element.transform.position + new Vector3(_lengthPlayingField + 1, 0, 0);
+            _grounds.Enqueue(element);
+        }
+    }
+
+    private GameObject GenerateGround(GameObject prefab, int x, int z)
     {
         GameObject spawned = Instantiate(prefab, _groundPool);
-        spawned.transform.position = new Vector3(x, _groundPool.position.y + y, z);
+        spawned.transform.position = new Vector3(x, _groundPool.position.y, z);
         return spawned;
-    }
-
-    private void SetupVariables()
-    {
-        _startX = (_x / 3) * -1;
-        _startZ = (_z / 3) * -1;
-        _x += _startX;
-        _z += _startZ;
-        _deltaX = _x + (-_startX);
-        _lastX = _x;
-    }
-
-    private HashSet<int> FillPosition(HashSet<int> positions, int count)
-    {
-        HashSet<int> timeHashSet = new HashSet<int>();
-        for (int i = 0; i < count; i++)
-        {
-            int elem = Random.Range(0, _deltaX * (_z + (-_startZ)));
-            while (positions.Contains(elem))
-                elem = Random.Range(0, _deltaX * (_z + (-_startZ)));
-
-            timeHashSet.Add(elem);
-        }
-
-        return timeHashSet;
-    }
-
-    private int GenerateNewZPosition(HashSet<int> positions)
-    {
-        int position = Random.Range(_startZ, _z);
-        while (positions.Contains(position))
-            position = Random.Range(_startZ, _z);
-
-        positions.Add(position);
-        return position;
-    }
-
-    private void MoveTailToHead(Queue<GameObject> queue, float zPosition)
-    {
-        GameObject element = queue.Dequeue();
-        element.transform.position = new Vector3(_lastX, element.transform.position.y, zPosition);
-        element.SetActive(true);
-        queue.Enqueue(element);
     }
 }
