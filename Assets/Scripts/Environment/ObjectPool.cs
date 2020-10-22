d52413a8e409dc3987920f3361eb072f0b0b5f8b
Oleg Anohin <oanohin@outlook.com>
2020-10-22 23:41:42 +0400

    Fix Errors v.003

---
+++ Assets/Scripts/Environment/ObjectPool.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public abstract class ObjectPool : MonoBehaviour
+{
+    [SerializeField] private GameObject _container;
+    [SerializeField] private int _capacity;
+
+    protected GameObject Container => _container;
+
+    private List<GameObject> _pool = new List<GameObject>();
+    private Camera _camera;
+
+
+    protected void Initialize(GameObject prefab)
+    {
+        _camera = Camera.main;
+
+        for (int i = 0; i < _capacity; i++)
+        {
+            GameObject spawned = Instantiate(prefab, _container.transform);
+            spawned.SetActive(false);
+            _pool.Add(spawned);
+        }
+    }
+
+    protected void Initialize(GameObject[] prefabs)
+    {
+        for (int i = 0; i < _capacity; i++)
+        {
+            int index = Random.Range(0, prefabs.Length);
+            GameObject spawned = Instantiate(prefabs[index], _container.transform);
+            spawned.SetActive(false);
+
+            _pool.Add(spawned);
+        }
+    }
+
+    protected void DisableObjectAbroadScreen()
+    {
+        foreach (var item in _pool)
+        {
+            if (item.activeSelf == true)
+            {
+                Vector3 point = _camera.WorldToViewportPoint(item.transform.position);
+                if (point.x < -0.5)
+                {
+                    item.SetActive(false);
+                    item.transform.parent = Container.transform;
+                }
+            }
+        }
+    }
+
+    protected bool TryGetObject(out GameObject result)
+    {
+        result = _pool.FirstOrDefault(p => p.activeSelf == false);
+
+        return result != null;
+    }
+}
---
+++ Assets/Scripts/Environment/Spawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Spawner : ObjectPool
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private Transform _target;
+    [SerializeField] private int _widthSpawner;
+
+    private System.Random _random = new System.Random();
+    private Vector3 _offset;
+    int _nextSpawn = 1;
+
+    private void Start()
+    {
+        _offset = new Vector3(_spawnPoint.position.x - _target.position.x, _spawnPoint.position.y, _spawnPoint.position.z);
+        Initialize(_prefab);
+    }
+
+    private void Update()
+    {
+        _spawnPoint.position = new Vector3(_target.position.x, _offset.y, _offset.z);
+
+        if (_spawnPoint.transform.position.x >= _nextSpawn)
+        {
+            int pointX = (int)_spawnPoint.transform.position.x;
+            _nextSpawn = _random.Next(pointX + 2, pointX + 5);
+
+            GenerateNewBoulders();
+
+            DisableObjectAbroadScreen();
+        }
+    }
+
+    private void GenerateNewBoulders()
+    {
+        int generatesInOneCycle = _random.Next(0, (int)(_widthSpawner * 0.2f));
+        HashSet<int> positions = new HashSet<int>();
+        positions = FillPositions(positions, generatesInOneCycle);
+
+        foreach (int item in positions)
+        {
+            if (TryGetObject(out GameObject boulder))
+            {
+                boulder.SetActive(true);
+                _spawnPoint.position = new Vector3(_target.position.x + _offset.x, _offset.y, _offset.z + item);
+                boulder.transform.position = _spawnPoint.position;
+            }
+        }
+    }
+
+    private HashSet<int> FillPositions(HashSet<int> positions, int count)
+    {
+        HashSet<int> timeHashSet = new HashSet<int>();
+        for (int i = 0; i < count; i++)
+        {
+            int elem = GetRandomPlace();
+            while (positions.Contains(elem))
+                elem = GetRandomPlace();
+
+            timeHashSet.Add(elem);
+        }
+
+        return timeHashSet;
+    }
+
+    private int GetRandomPlace()
+    {
+        return Random.Range(-(_widthSpawner / 2), _widthSpawner / 2);
+    }
+}
